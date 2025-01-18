e2676a8b3f67a058db404c0d08479c74b470e8f5
bruderj15 <julian.bruder@outlook.com>
2025-01-18 12:16:05 +0100

    main: add SpringBootApplication main

---
+++ src/main/java/demo/modulith/DemoModulithEcommerceApplication.java
@@ -0,0 +1,13 @@
+package demo.modulith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoModulithEcommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoModulithEcommerceApplication.class, args);
+    }
+
+}
